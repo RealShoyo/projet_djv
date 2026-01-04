1230ba76bf23b30e60cbfb37d59648f57e4fc98a
Yannis Ladron <yannis2.ladron@gmail.com>
2026-01-04 03:14:23 +0100

    endscreen

---
+++ Assets/Scripts/EndScreenManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreenManager : MonoBehaviour
+{
+    
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+}
